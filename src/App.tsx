5a90b4ec726ffd7cd51dabf44e01fabf635f6984
Gail <gailh@google.com>
2021-08-28 18:51:31 -0700

    Add dance and jiggy boxes

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button id="circle" >g</button>
-      <div id="diamond">
-        <span id="i">a</span>
+
+      <div id="gail">
+        <button id="circle" >g</button>
+        <div id="diamond">
+          <span id="i">a</span>
+        </div>
+        <div id="rectangle">
+          i
+        </div>
+        <div id="triangle">l</div>
       </div>
-      <div id="rectangle">
-        i
+      <div className="dance">
+        <span className="d">d</span>
+        <span className="a">a</span>
+        <span className="n">n</span>
+        <span className="c">c</span>
+        <span className="e">e</span>
       </div>
-      <div id="triangle">l</div>
+      <div id="shiver">
+        gettin' jiggy
+      </div>
+
     </div>
   );
 }
